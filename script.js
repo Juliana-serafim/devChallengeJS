b88f3f5d6f8d595bfd0ecf46d4cb92be467368c2
Juliana <jss4@cin.ufpe.br>
2021-06-17 17:52:16 -0300

    feat(develop): buttons of ice-cream color with animations

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 function blueFunction() {
     const headercolor = document.getElementById('header');
     const footercolor = document.getElementById('footer');
-    const animation = document.getElementById('blue-animation');
+    const blue = document.querySelector('.blue' || '.blue-off');
+    const pink = document.querySelector('.pink');
+    const orange = document.querySelector('.orange');
     
       headercolor.style.background = "linear-gradient(180deg, rgba(81, 111, 251, 0.8) 44.17%, rgba(255, 255, 255, 0.12) 100%)";
       footercolor.style.background = "rgba(81, 111, 251, 0.56)";
-      animation.style.paddingBottom = "10px";
+      blue.classList.add('blue-click');
+      blue.classList.remove('blue-off');
+      pink.classList.add('pink-off');
+      orange.classList.add('orange-off');
+      
 }
 
 function pinkFunction() {
-   const headercolor = document.getElementById('header');
-   const footercolor = document.getElementById('footer');
-   const animationpink = document.getElementById('pink-animation');
+    const headercolor = document.getElementById('header');
+    const footercolor = document.getElementById('footer');
+    const bluefp = document.querySelector('.blue'||'.blue-click');
+    const pinkfp = document.querySelector('.pink'||'.pink-off');
+    const orangefp = document.querySelector('.orange' || '.orange-click');
 
-   headercolor.style.background = "linear-gradient(180deg, #FF99C4 44.17%, rgba(255, 255, 255, 0.12) 100%)";
+    headercolor.style.background = "linear-gradient(180deg, #FF99C4 44.17%, rgba(255, 255, 255, 0.12) 100%)";
     footercolor.style.background = "rgba(244, 155, 140, 0.56)";
-    animationpink.style.paddingBottom = "10px";
+    bluefp.classList.add('blue-off');
+    pinkfp.classList.add('pink-click');
+    pinkfp.classList.remove('pink-off')
+    orangefp.classList.add('orange-off');
     
 }
   
 function orangeFunction() {
     const headercolor = document.getElementById('header');
     const footercolor = document.getElementById('footer');
-    const animationorange = document.getElementById('orange-animation');
+    const bluefo = document.querySelector('.blue');
+    const pinkfo = document.querySelector('.pink');
+    const orangefo = document.querySelector('.orange'||'.orange-off');
     
     headercolor.style.background = "linear-gradient(180deg, rgba(240, 94, 43, 0.8) 44.17%, rgba(255, 255, 255, 0.12) 100%)";
     footercolor.style.background = "rgba(240, 94, 43, 0.56)";
-    animationorange.style.paddingBottom = "10px";
+    bluefo.classList.add('blue-off');
+    pinkfo.classList.add('pink-off');
+    orangefo.classList.add('orange-click');
+    orangefo.classList.remove('orange-off');
     
-}
+} 
 
 function pesquisar(){
     const pesquisa = document.querySelector('.barra-pesquisa');
